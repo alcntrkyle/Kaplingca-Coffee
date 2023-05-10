212976f16ba71488bde03e7ff3f26271e002ad2d
kylealcantara <kylealcantara0628@gmail.com>
2023-05-11 02:41:13 +0800

    first commit

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+function showCardDetails() {
+  let cardDetails = document.getElementById("card-det");
+  let selectedMOP = document.querySelector('input[name="optradio"]:checked').value;
+
+  if (selectedMOP === "cash") {
+    cardDetails.style.display = "none";
+  } else if (selectedMOP === "credit") {
+    cardDetails.style.display = "";
+  } else {
+    cardDetails.style.display = "none";
+  }
+}
